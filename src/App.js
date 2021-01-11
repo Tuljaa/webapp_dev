a6f0fcb6964c7d819777653ce892f25f7fb95c39
Tulja Bhavani Kshatri <tuljakshatri@Tuljas-MacBook-Air.local>
2021-01-11 15:01:11 +0530

    testing with Jest

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React, { useState} from "react";
 import Weather from './components/Weather.js';
 
-function App() {
+const App=() => {
 
   const APIKEY="8c692ded13dc3e125587edd1d73ef8e1";
 
@@ -10,7 +10,7 @@ function App() {
     city:"",
     country:""
   })
-  const [weatherdata, setweatherdata] = useState([])
+  const [weather, setweather] = useState([ ])
 
   const handleChange=(e)=>{
     let name=e.target.name;
@@ -22,35 +22,29 @@ function App() {
       setstate({...Form, country:value})
     }
   }
-
   async function apiData(e){
     e.preventDefault();
     if(Form.city===""){
       alert("Please enter the city name");
     }
     else{
-      console.log(weatherdata);
       const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${Form.city}&appid=${APIKEY}`)
       const jsonobj= await data.json();
-      
-     //console.log(jsonobj);
-     //alert(jsonobj.main.temp);
-      setweatherdata(jsonobj.main);
-      console.log(weatherdata);
+      setweather({data:jsonobj.main});
       
     }
   } 
   return (
     <div className="App ">
       <h2 className="jumbotron font-weight-bold"> Weather Report Application</h2>
-      <form>
-        <input type="text" name="city" onChange={(e)=>handleChange(e)} placeholder="Enter City Name" className="form-control active required"/><br></br>
-        <input type ="text" name="country" onChange={(e)=>handleChange(e)} placeholder="Enter Country Name" className="form-control required"/><br></br>
+      <form className="form-group">
+        <input type="text" name="city" onChange={(e)=>handleChange(e)} placeholder="Enter City Name" className="form-control active "/><br></br>
+        <input type ="text" name="country" onChange={(e)=>handleChange(e)} placeholder="Enter Country Name" className="form-control"/><br></br>
         <button type="submit" onClick={(e)=>apiData(e)} className="form-control btn-outline-info"> Submit</button>
       </form>
       {
-      ( (weatherdata!==undefined)? <div>
-      < Weather data={weatherdata}/>  </div> 
+      ( (weather.data!==undefined)? <div>
+      < Weather data={weather.data}/>  </div> 
       : null )
     }
     </div>
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,15 +1,13 @@
 import React from 'react'
 
 function Weather(props) {
-    console.log("IN WEATHRE JS ",props.data.temp);
-   //const {data}=props.data;
     return (
         <div>
-            <ol className="mt-4 font-weight-light"> 
-                <li className="p-1">Temparature = {props.data.temp}</li>
-                <li className="p-1">Feels Like = {props.data.feels_like}</li>
-                <li className="p-1">Temparature Minimum = {props.data.temp_min}</li>
-                <li className="p-1"> Temparature Maximum = {props.data.temp_max}</li>
+            <ol className="list-group-horizontal  mt-4"> 
+                <li className="list-group-item list-group-item-success p-1">Temparature = {props.data.temp}</li>
+                <li className="list-group-item list-group-item-info p-1">Feels Like = {props.data.feels_like}</li>
+                <li className="list-group-item list-group-item-warning p-1">Temparature Minimum = {props.data.temp_min}</li>
+                <li className="list-group-item list-group-item-danger p-1"> Temparature Maximum = {props.data.temp_max}</li>
             </ol>
         </div>
     );
