882d41922be0105237c6630daf263f3c32f07c5e
Tulja Bhavani Kshatri <tuljakshatri@Tuljas-MacBook-Air.local>
2021-01-25 19:31:35 +0530

    Done with UI

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Weather from './components/Weather.js';
 
 const App=() => {
 
-  const APIKEY="8c692ded13dc3e125587edd1d73ef8e1";
+  //const APIKEY="8c692ded13dc3e125587edd1d73ef8e1";
+   const APIKEY="fbf712a5a83d7305c3cda4ca8fe7ef29";
 
   const [Form, setstate] = useState({
     city:"",
@@ -22,22 +23,34 @@ const App=() => {
       setstate({...Form, country:value})
     }
   }
+
   async function apiData(e){
-    e.preventDefault();
-    
-      const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${Form.city}&appid=${APIKEY}`)
-      const jsonobj= await data.json();
-      setweather({data:jsonobj.main});
+      e.preventDefault();
+      if(Form.city==="" || Form.country===""){
+        alert("Enter values");
+          return
+      }
+      const data= await fetch(` https://api.openweathermap.org/data/2.5/forecast?q=${Form.city}&units=metric&appid=${APIKEY}`)
+      //const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${Form.city}&appid=${APIKEY}`)
+      const jsonobj= await data.json(); 
+      //alert(jsonobj);
+      setweather({data:jsonobj});
+      
       } 
+
   return (
     <div className="App" data-test="App">
-      <h2 className="jumbotron font-weight-bold" data-test="header"> Weather Report Application</h2>
-      <form className="form-group" data-test="form">
-        <input type="text" data-test="cityN" name="city" value onChange={(e)=>handleChange(e)} placeholder="Enter City Name" className="form-control active " /><br></br>
-        <input type ="text" name="country" value onChange={(e)=>handleChange(e)} placeholder="Enter Country Name" className="form-control" data-test="countryN"/><br></br>
-        <button type="submit" onClick={(e)=>apiData(e)} className="form-control btn-outline-info" data-test="submit"> Submit</button>
+      <h2 className=" p-4 display-4" data-test="header"> Weather Forecast</h2><hr className="horizontal"></hr>
+      <form className="form-inline ml-5" data-test="form">
+        <label className="text-white"> <strong> City Name : </strong> </label>
+        <input type="text" data-test="cityN" name="city" onChange={(e)=>handleChange(e)} placeholder="Enter City Name" className="form-control border border-dark" />
+        <label className="text-white"> <strong>Country Name : </strong></label>
+        <input type ="text" name="country" onChange={(e)=>handleChange(e)} placeholder="Enter Country Name" className="form-control shadow-sm " data-test="countryN"/><br></br>
+        <button type="submit" onClick={(e)=>apiData(e)} className="form-control btn-outline-info p-2 m-4 shadow bg-black text-dark" data-test="submit"> <strong>Submit </strong> </button>
       </form>
+    <hr ></hr>   
       {
+        
       ( (weather.data!==undefined)? <div>
       < Weather data={weather.data}/>  </div> 
       : null )
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,15 +1,151 @@
 import React from 'react'
+import '../App.css';
+import icon from '../cloudy.png';
+import location from '../placeholder.png';
 
 function Weather(props) {
+
+    var styles = {
+        'padding':'10px',
+        width: '100px',
+        height: '100px',
+      };
+
+      var stylesImg = {
+        margin: '10px',
+        width: '30px',
+        height: '30px',
+        display: 'inline-block',
+      };
+
+      var v1={
+        'border-left': '1px solid black',
+        height: '100px',
+        'margin-left':'10px',
+        'margin-top':'-100px',
+      }
+          
+      const time=props.data.city.timezone/3600;
+      const d = new Date();
+      const utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+     const nd = new Date(utc + (3600000*time)); 
+
     return (
-        <div>
-            <ol className="list-group-horizontal  mt-4" data-test="orderedlist"> 
-                <li className="list-group-item list-group-item-success p-1" data-test="listitem">Temparature = {props.data.temp} K</li>
-                <li className="list-group-item list-group-item-info p-1" data-test="listitem">Feels Like = {props.data.feels_like} K</li>
-                <li className="list-group-item list-group-item-warning p-1" data-test="listitem">Temparature Minimum = {props.data.temp_min} k</li>
-                <li className="list-group-item list-group-item-danger p-1" data-test="listitem"> Temparature Maximum = {props.data.temp_max} k</li>
-            </ol>
+
+    <div >
+
+               <div className="d-inline-block  m-5" hourly>
+               <img src={location} alt="Icon" style={stylesImg} ></img><br></br>
+                <h3 >{props.data.city.name} <span>{props.data.city.country}</span></h3>
+                </div>
+            
+                <div className="d-inline-block m-5" hourly>
+                <img src={icon} alt="Icon" style={styles} ></img><br></br>
+                <h1 className="display-4 d-inline-flex ">{props.data.list[0].main.temp} <span>&#8451;</span></h1>
+                <h5><strong>{nd.toLocaleString()}</strong></h5>
+                </div>
+
+                <br></br><hr></hr>
+
+            <h5 className="p-3">HOURLY</h5>
+
+            <div className="d-inline-block hourly ">
+                <img src={icon} alt="Icon" style={stylesImg}></img>
+                <h5>{props.data.list[1].main.temp}<span>&#8451;</span></h5>
+                <h5>{props.data.list[1].dt_txt}</h5>
+               
+            </div>
+
+            <div className="d-inline-block hourly" >
+                <img src={icon} alt="Icon" style={stylesImg}></img>
+                <h5>{props.data.list[2].main.temp}<span>&#8451;</span></h5>
+                <h5>{props.data.list[2].dt_txt}</h5>
+         
+                <div style={v1}></div>
+            </div>
+
+            <div className="d-inline-block hourly" >
+                <img src={icon} alt="Icon" style={stylesImg}></img>
+                <h5>{props.data.list[3].main.temp}<span>&#8451;</span></h5>
+                <h5>{props.data.list[3].dt_txt}</h5>
+               
+                <div style={v1}></div>
+            </div>
+            <br></br><hr ></hr>
+
+            <h5 >DAILY</h5><br></br>
+
+       
+       <div className="flip-card hourly d-inline-block">
+            <div className="flip-card-inner">
+                <div className="flip-card-front">
+                <img src={icon} alt="Icon" style={stylesImg}></img>
+                <h5>{props.data.list[4].main.temp}<span>&#8451;</span></h5>
+                <h5>{props.data.list[4].dt_txt}</h5>
+                </div>
+                <div className="flip-card-back">
+                <div className="d-inline-block  " >
+                <img src={icon} alt="Icon" style={stylesImg}></img>
+                <h5>{props.data.list[4].main.temp}<span>&#8451;</span></h5>
+                <h5>{props.data.list[4].dt_txt}</h5>
+               </div>
+                </div>
+            </div>
+            </div>
+
+            <div className="flip-card d-inline-block">
+            <div className="flip-card-inner">
+                <div className="flip-card-front">
+                <img src={icon} alt="Icon" style={stylesImg}></img>
+                <h5>{props.data.list[4].main.temp}<span>&#8451;</span></h5>
+                <h5>{props.data.list[4].dt_txt}</h5>
+                </div>
+                <div className="flip-card-back">
+                <div className="d-inline-block hourly " >
+                <img src={icon} alt="Icon" style={stylesImg}></img>
+                <h5>{props.data.list[4].main.temp}<span>&#8451;</span></h5>
+                <h5>{props.data.list[4].dt_txt}</h5>
+               </div>
+                </div>
+            </div>
+            </div>
+
+            <div className="flip-card d-inline-block">
+            <div className="flip-card-inner">
+                <div class="flip-card-front">
+                <img src={icon} alt="Icon" style={stylesImg}></img>
+                <h5>{props.data.list[4].main.temp}<span>&#8451;</span></h5>
+                <h5>{props.data.list[4].dt_txt}</h5>
+                </div>
+                <div className="flip-card-back">
+                <div className="d-inline-block hourly " >
+                <img src={icon} alt="Icon" style={stylesImg}></img>
+                <h5>{props.data.list[4].main.temp}<span>&#8451;</span></h5>
+                <h5>{props.data.list[4].dt_txt}</h5>
+               </div>
+                </div>
+            </div>
+            </div>
+
+            <div className="flip-card d-inline-block">
+            <div className="flip-card-inner">
+                <div className="flip-card-front">
+                <img src={icon} alt="Icon" style={stylesImg}></img>
+                <h5>{props.data.list[4].main.temp}<span>&#8451;</span></h5>
+                <h5>{props.data.list[4].dt_txt}</h5>
+                </div>
+                <div className="flip-card-back">
+                <div className="d-inline-block hourly " >
+                <img src={icon} alt="Icon" style={stylesImg}></img>
+                <h5>{props.data.list[4].main.temp}<span>&#8451;</span></h5>
+                <h5>{props.data.list[4].dt_txt}</h5>
+               </div>
+                </div>
+            </div>
+            </div>
+
         </div>
+       
     );
 }
 
